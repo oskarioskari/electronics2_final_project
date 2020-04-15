98a7219393ac7fb3043274361962bf6a6230fb8d
Tommiska <tommiosk@ad.helsinki.fi>
2020-04-15 13:20:47 +0300

    Merge different versions
    Moved the edited file into src/main/
    
    Co-Authored-By: jontza <jontza@users.noreply.github.com>

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,6 +1,10 @@
 #include <Servo.h>
 #include "SR04.h"
 
+#include <ESP8266_Lib.h>
+#include <BlynkSimpleShieldEsp8266.h>
+#include <SoftwareSerial.h>
+
 Servo servo;
 Servo l_motor;
 Servo r_motor;
@@ -17,6 +21,19 @@ int l_speed = 90; // Left motor speed.
 int distance = 100;
 int dir = 1;
 int stuck = 0;
+int rc = 0;
+
+//Blynk Authorization Token
+char auth[] = "XbCSkVzfzfNctejvsA_u0yJoarWFOFfn";
+
+// WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "HONOR 20";
+char pass[] = "12345678";
+
+// Your ESP8266 baud rate:
+#define ESP8266_BAUD 9600
+#define BLYNK_PRINT Serial
 
 // Pin numbers:
 int servoPin = 9;  // PIN: Servo
@@ -24,8 +41,14 @@ int echoPin = 11;  // PIN: US-Sensor Echo
 int trigPin = 12;  // PIN: US-Sensor Trig
 int lMotorPin = 5; // PIN: Left Motor
 int rMotorPin = 6; // PIN: Right Motor
+int rxPin = 2;     // PIN: RX
+int txPin = 3;     // PIN: TX
 
 SR04 sr04 = SR04(echoPin, trigPin);
+
+SoftwareSerial EspSerial(rxPin, txPin);
+
+ESP8266 wifi(&EspSerial);
 
 void setup() {
   servo.attach(servoPin);
@@ -34,32 +57,41 @@ void setup() {
   servo.write(pos);
   l_motor.write(l_speed);
   r_motor.write(r_speed);
-  Serial.begin(115200);
+  Serial.begin(9600);
+  
+  EspSerial.begin(9600);
+  delay(10);
+  
+  Blynk.begin(auth, wifi, ssid, pass);
+  
   Serial.println("Started");
 }
 
 void loop() {
-  delay(2000); // TODO: This might not be needed. Useful when debugging.
+  delay(100); // TODO: This might not be needed. Useful when debugging.
 
+  Blynk.run();
+  
   distance = sr04.Distance();
   Serial.print("Distance: ");
   Serial.println(distance);
-
-  if (distance <= collision || stuck == 1) { // Check if there are obstacles too close or if the car is stuck.
-    moveStop();
-    dir = scanDirections();
-    if (dir == -1) {
-      stuck = 0;
-      turnLeft();
-    } else if (dir == 1) {
-      stuck = 0;
-      turnRight();
-    } else if (dir == 0) { // If stuck, reverse.
-      stuck = 1;
-      moveBackward();
+  if (rc == 0) {  // Check is the car being controlled remotely
+    if (distance <= collision || stuck == 1) { // Check if there are obstacles too close or if the car is stuck.
+      moveStop();
+      dir = scanDirections();
+      if (dir == -1) {
+        stuck = 0;
+        turnLeft();
+      } else if (dir == 1) {
+        stuck = 0;
+        turnRight();
+      } else if (dir == 0) { // If stuck, reverse.
+        stuck = 1;
+        moveBackward();
+      }
+    } else {
+      moveForward();
     }
-  } else {
-    moveForward();
   }
 }
 
@@ -140,6 +172,7 @@ void turnRight() {
   delay(turnDelay);
   moveStop();
 }
+
 void turnLeft() {
   Serial.println("Turn Left");
   l_speed = 90;
@@ -147,4 +180,52 @@ void turnLeft() {
   moveCar();
   delay(turnDelay);
   moveStop();
+}
+
+// Here starts the remote controlling code
+
+BLYNK_WRITE(V1) { // Drive forward
+ if (param.asInt() == 1) {
+    l_speed = 150;
+    r_speed = 30;
+    rc = 1; 
+    Serial.println("Button pressed");
+  }else{
+    Serial.println("Button not pressed");
+    moveStop();
+    rc = 0;
+  }
+}
+
+BLYNK_WRITE(V2) { // Turn right
+ if (param.asInt() == 1) {
+    l_speed = 150;
+    r_speed = 90; 
+    rc = 1;
+  }else{
+    moveStop();
+    rc = 0;
+  }
+}
+
+BLYNK_WRITE(V3) { // Drive backward
+ if (param.asInt() == 1) {
+    l_speed = 30;
+    r_speed = 150;
+    rc = 1;
+  }else{
+    moveStop();
+    rc = 0;
+  }
+}
+
+BLYNK_WRITE(V4) { // Turn left
+ if (param.asInt() == 1) {
+    l_speed = 90;
+    r_speed = 30;
+    rc = 1; 
+  }else{
+    moveStop();
+    rc = 0;
+  }
 }
